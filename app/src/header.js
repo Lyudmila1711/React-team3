eeb62c8425c9fe253291328ca954ffa8537d9f41
Orestvahila <orest.vahila@gmail.com>
2022-01-31 23:45:25 +0200

    first commit

---
+++ app/src/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+import Img from './Subaru.jfif'
+
+
+const HeaderBlock = () => (
+    <div class='header'>
+        <div>
+            <img src={Img} alt="aaa" ></img>
+            </div>
+        <div>
+            <h1>Heading</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat</p>
+        </div>
+    </div>
+)
+
+export default  HeaderBlock;
+
---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import HeaderBlock from "./header";
+import './header.css'
+
+ReactDOM.render(
+    <div className='main'>
+      <HeaderBlock />,
+    </div>,
+    document.getElementById('root')
+  );
