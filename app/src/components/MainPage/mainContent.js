633cdbe692dae032510bc98af8dbe72439b3e867
Orestvahila <orest.vahila@gmail.com>
2022-02-02 22:13:18 +0200

    added some style in footer header and main

--- app/src/components/MainPage/mainContent.js
+++ app/src/components/MainPage/mainContent.js
@@ -3,11 +3,21 @@ import '../../styles/mainContent.css'
 import legoDuplo from '../../img/lego-duplo.jpeg'
 import legoIkea from '../../img/lego-ikea.jpeg'
 import legoLgbt from '../../img/lego-lgbt.jpeg'
+import headingImg from '../../img/heading img.jfif'
 
 
 function MainContent (){
     return (
     <main>
+        <div className='heading'>
+            <div>
+                <img src={headingImg} alt="image" ></img>
+            </div>
+            <div>
+                <h1>Heading</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat</p>
+            </div>
+        </div>
         <div className='main'>
         <div>
             <img src={legoDuplo} alt = 'image of lego duplo'></img>
